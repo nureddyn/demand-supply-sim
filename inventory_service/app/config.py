f048d4d1d5a73f5df802ccc5d387a062989ebcf9
nureddyn <nureddynballout@gmail.com>
2025-10-15 19:03:38 -0400

    Add database service and its container configuration
    - Ignore env files and bytecode files
    - Add db service to docker-compose.yml
    - Rearange folder structure
    - Add config file for db connection

---
+++ inventory_service/app/config.py
@@ -0,0 +1,18 @@
+import os
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    database_url: str
+    env: str = "development"
+
+    class Config:
+        env_file = f".env.{os.getenv('ENV', 'development')}"
+        env_file_encoding = 'utf-8'
+
+settings = Settings()
+
+"""
+Usage:
+from config import settings
+DATABASE_URL = settings.database_url
+"""
